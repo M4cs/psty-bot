aca1c5b4367f764f776bd2c09e181a4f613c3af6
M4cs <mabridgland@protonmail.com>
2019-12-17 20:20:32 -0500

    Listener Update

--- plugins/listener.py
+++ plugins/listener.py
@@ -12,7 +12,7 @@ class Listener(Plugin):
         print(event.message)
         urls = []
         if len(event.raw_data['message']['attachments']) != 0:
-            for attachment in event.message.raw_data['message']['attachments']:
+            for attachment in event.raw_data['message']['attachments']:
                 urls.append(attachment['url'])
         for url in urls:
             paster.upload_file(url)

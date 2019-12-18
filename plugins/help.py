3e7ed825e0c02b81feb9b191ea2e1d6b7d9b0299
M4cs <mabridgland@protonmail.com>
2019-12-17 19:38:34 -0500

    Fix Help

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from disco.bot import Plugin
 class Help(Plugin):
     @Plugin.listen('MessageCreate')
     def on_message_create(self, event):
-        if event.message.content == "<@656638609998217266> help":
+        if event.message.content == "@psty.io help":
             event.msg.reply("""$paste <language or plaintext> <content>""")

72b78c968ba20e3e80f75edbcdc57a2a46d57c22
M4cs <mabridgland@protonmail.com>
2019-12-17 19:37:44 -0500

    Fix Indent

--- plugins/help.py
+++ plugins/help.py
@@ -1,7 +1,7 @@
 from disco.bot import Plugin
 
 class Help(Plugin):
-     @Plugin.listen('MessageCreate')
+    @Plugin.listen('MessageCreate')
     def on_message_create(self, event):
         if event.message.content == "<@656638609998217266> help":
             event.msg.reply("""$paste <language or plaintext> <content>""")
